c139669de641cb5adf095f2e09b0298ee6de3ef1
= <mturzynskanj@gmail.com>
2023-09-26 14:39:34 -0400

    Resume page updates

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,132 +1,261 @@
+import React from "react";
 
-
-
-import React from 'react'
-
-const  Resume = ()=> {
-    return (
-    <section className='page'>
-    <div>
-    <section className="resume">
-        <section className="panel">
+const Resume = () => {
+  return (
+    <section className="page">
+      <div>
+        <section className="resume">
+          <section className="panel">
             <h2>RESUME</h2>
             <h3>MARIA TURZYNSKA</h3>
             <p> 0-126 Yerger Rd.</p>
-            <p>  Fair Lawn, NJ 07410 </p>
-            <p>tel: (201) 300-6127   cell: (201) 398-8954</p>
-            <p> email: mturzynskanj@gmail.com  </p>
-        </section>
+            <p> Fair Lawn, NJ 07410 </p>
+            <p>tel: (201) 300-6127 cell: (201) 398-8954</p>
+            <p> email: mturzynskanj@gmail.com </p>
+          </section>
 
-        <section className="panel">
-            <h3><strong>SUMMARY</strong></h3>
-            <p>User  interface  developer/programmer with experience using object oriented design technology and programming languages. Skilled in designing and building web pages using a variety of graphics software applications as well as hand coding.
-                Broad  knowledge of available web development technologies, frameworks and libraries. </p>
-        </section>
+          <section className="panel">
+            <h3>
+              <strong>SUMMARY</strong>
+            </h3>
+            <p>
+              User interface developer/programmer with experience using object
+              oriented design technology and programming languages. Skilled in
+              designing and building web pages using a variety of graphics
+              software applications as well as hand coding. Broad knowledge of
+              available web development technologies, frameworks and libraries.{" "}
+            </p>
+          </section>
 
-        <section className="panel extra-spacing ">
+          <section className="panel extra-spacing ">
             <h3>TECHNICAL SKILLS</h3>
-            <p><strong>Programming Languages and Libraries:</strong><br />
-                Javascript, React.js, React Query, Redux.js, React Router 6, Rxjs, Npm, Node.js, Next.js, Ag-grid<br />
-                CSS3, Flexbox, Sass, JSS, Material UI <br ></br>
-                D3.js<br />
+            <p>
+              <strong>Programming Languages and Libraries:</strong>
+              <br />
+              Javascript, Rxjs, Npm, Node.js, Next.js, Ag-grid <br />
+              React.js, React Hooks, React Query, React Router 6, Redux.js,
+              Jest, React Testing Library <br />
+              CSS3, Flexbox, Sass, JSS, Material UI <br />
+              D3.js
+              <br />
             </p>
 
-            <p><strong>Application Build and Task Tools:</strong><br />
-                Webpack, Jenkins, Github
+            <p>
+              <strong>Application Build and Task Tools:</strong>
+              <br />
+              Webpack, Jenkins, Github
             </p>
-            <p ><strong>Operating Systems:</strong><br />Windows, Mac Os</p>
-<p ><strong>Software:</strong><br />Visual Studio Code, Charles Web Debugging Proxy, Postman</p>
- 
-       
-        
-        <section className="panel">
-            <h3>EDUCATION</h3>
-            <p>Masters in Computer Science, Lehman College, New York</p>
-            <p> Bachelors of Engineering, Polytechnic University of Gdansk, Poland</p>
+            <p>
+              <strong>Operating Systems:</strong>
+              <br />
+              Windows, Mac Os
+            </p>
+            <p>
+              <strong>Software:</strong>
+              <br />
+              Visual Studio Code, Charles Web Debugging Proxy, Postman
+            </p>
+
+            <section className="panel">
+              <h3>EDUCATION</h3>
+              <p>Masters in Computer Science, Lehman College, New York</p>
+              <p>
+                {" "}
+                Bachelors of Engineering, Polytechnic University of Gdansk,
+                Poland
+              </p>
+            </section>
+            <section className="panel">
+              <h3>EXPERIENCE</h3>
+
+              <h4 className="mar-tb-15">
+                Royal Bank of Canada, July 2018 - July 2023
+              </h4>
+              <p>
+                Transitioned multiple applications from a legacy system to a
+                modern HTML5 stack: React.js, Redux.js, Material UI, Node.js
+              </p>
+              <p>
+                Designed, and implemented a front-end trading application with
+                high-frequency real-time grid updates.
+              </p>
+              <p>
+                Advocated a configuration driven UI development (CDD) which
+                allows the customization of the UI for different user cases or
+                user groups without extensive code changes.{" "}
+              </p>
+              <p>
+                Utilized ag-grid library to build high-performance interactive
+                grids with many customized features - cell editor, cell
+                renderers, filters, grouping, tool panels, tooltips, and custom
+                views.
+              </p>
+              <p>
+                Worked with Solace Event Broker for real-time cross applications
+                messaging.
+              </p>
+              <p>
+                Utilized OpenFin Api (Bus, Channels) for real-time data passing
+                between applications, openfin notifications, and custom openfin
+                windows.{" "}
+              </p>
+              <p>
+                Build reusable react components as npm modules to be consumed
+                across multiple applications to ensure clean and consistent UI
+                interfaces.
+              </p>
+
+              <h4 className="mar-tb-15">
+                WebMD, NYC, January 2016 - October 2017
+              </h4>
+              <p>Built responsive video websites with Akamai Video Platform</p>
+              <p>Extended existing platform with new AMD modules</p>
+              <p>
+                Optimized grunt build proccess with r.js to combine javascript
+                files into one file to improve website performance
+              </p>
+              <p>Built object oriented jquery plugins</p>
+              <p>
+                Helped shape design decisions related to performance and user
+                experience{" "}
+              </p>
+              <p>Created comprehensive products documentation on Confluence </p>
+
+              <h4 className="mar-tb-15">
+                Federal Reserve Bank, NYC, November 2014 - August 2015
+              </h4>
+              <p>
+                Built responsive web application with Backbone Marionette and
+                D3.js library
+              </p>
+              <p>
+                {" "}
+                Built prototype application for mobile devices with Backbone and
+                D3.js tree layout api
+              </p>
+              <p>Upgraded internal framework with Bootstrap 3 </p>
+
+              <h4 className="mar-tb-15">
+                Institute for Integrative Nutrition, New York, User Interface
+                Engineer, November 2013 - August 2014
+              </h4>
+
+              <p>
+                Created responsive website using Bootstrap 3, Less and jQuery
+              </p>
+              <p>
+                Utilized google analytics API to generate page views, events and
+                ecommerce transactions reports
+              </p>
+              <p>Utilized social media API ( facebook, twiter)</p>
+              <p>
+                Implemented client side form validation with Angular directives
+              </p>
+              <p>Used GIT versioning system</p>
+
+              <h4 className="mar-tb-15">
+                Equinox , New York, User Interface developer , August 2010 -
+                October 2013
+              </h4>
+
+              <p>
+                Created mobile website with Responsive Web Design using
+                Bootstrap 3
+              </p>
+              <p>
+                Implemented restful modular application using javascript
+                Backbone MVC framework, Grunt and Grunt-bbb
+              </p>
+              <p>
+                Utilized various HTML5 API’s - local storage, drag and drop,
+                offline mode{" "}
+              </p>
+              <p>Created jQuery object oriented plugins</p>
+              <p>
+                Implemented javascript publisher/subscriber pattern for very
+                rich UI application
+              </p>
+              <p>
+                Transformed PSD’s into cross browser compatible and valid
+                HTML5/CSS3
+              </p>
+              <p>Created animated widgets with Raphael JS Library</p>
+              <p>
+                Insured cross browser and device compatibility – for Windows and
+                Mac environments
+              </p>
+              <p>
+                Created html emails that render properly in desktop and mobile
+                devices
+              </p>
+
+              <h4 className="mar-tb-15">
+                Redcats USA, New York UI developer , February 2008 - March 2010{" "}
+              </h4>
+
+              <p>
+                Developed front-end web technologies that implemented a
+                high-quality user experience for European e-commerce websites
+                designed in .NET environment.
+              </p>
+              <p>
+                Transformed design mockup into cross browser compatible and
+                valid XHTML/CSS layouts and implemented dynamic elements using
+                JavaScript, AJAX and Jquery{" "}
+              </p>
+              <p>
+                Analyzed web site response time and explored different factors
+                that affect web page speed. Worked with a team on optimizing
+                structure, design and graphics of the websites to improve
+                response time.{" "}
+              </p>
+
+              <h4 className="mar-tb-15">
+                {" "}
+                Hertz, Park Ridge, NJ, UI developer, June 2007 - January 2008
+              </h4>
+
+              <p>
+                Designed and updated web sites using Dreamweaver, CSS2, DOM and
+                JavaScript
+              </p>
+              <p>Converted html code to valid and well formed xhtml</p>
+              <p>
+                Tested websites on multiple browsers and corrected CSS code to
+                achieve cross compatibility
+              </p>
+
+              <h4 className="mar-tb-15">
+                Freelance UI Developer (2006 - 2007)
+              </h4>
+
+              <p>
+                Created and updated web sites using Dreamweaver MX, JavaScript ,
+                Photoshop.{" "}
+              </p>
+              <p>
+                Created artwork, digitally modified photography, and optimized
+                images.
+              </p>
+              <p>
+                Used CSS to control the presentation of the website for
+                different media.
+              </p>
+              <p>Designed animated banners by utilizing Flash MX </p>
+
+              <h4 className="mar-tb-15">
+                Assistant, Lehman College Spring 2003
+              </h4>
+              <p>
+                Responsibilities included mentoring Students, tutoring Java and
+                helping students with troubleshooting assignments.
+              </p>
+            </section>
+          </section>
         </section>
-        <section className="panel">
-            <h3>EXPERIENCE</h3>
-
-            
-            <h4 className="mar-tb-15">Royal Bank of Canada</h4>
-            <p>Transitioned multiple applications from a legacy system to a modern HTML5 stack: React.js, Redux.js, Material UI, Node.js</p>
-            <p>Designed, and implemented a front-end trading application with high-frequency real-time grid updates.</p>
-            <p>Advocated a configuration driven UI development (CDD) which allows the customization of the UI for different user cases or user groups without extensive code changes. </p>
-            <p>Utilized ag-grid library to build high-performance interactive grids with many customized features - cell editor, cell renderers, filters, grouping, tool panels, tooltips, and custom views.</p>
-            <p>Worked with Solace Event Broker for real-time cross applications messaging.</p>
-            <p>Utilized OpenFin Api (Bus, Channels) for real-time data passing between applications, openfin notifications, and custom openfin windows. </p>
-            <p>Build reusable react components as npm modules to be consumed across multiple applications to ensure clean and consistent UI interfaces.</p>
-    
-            <h4 className="mar-tb-15">WebMD, NYC, January 2016 - October 2017</h4>
-            <p>Built responsive video websites with Akamai Video Platform</p>
-            <p>Extended existing platform with new AMD modules</p>
-            <p>Optimized grunt build proccess with r.js to combine javascript files into one file to improve website performance</p>
-            <p>Built object oriented jquery plugins</p>
-            <p>Helped shape design decisions related to performance and user experience </p>
-            <p>Created comprehensive products documentation on Confluence </p>
-    
-            <h4 className="mar-tb-15">Federal Reserve Bank, NYC, November 2014 - August 2015</h4>
-            <p>Built responsive web application with Backbone Marionette and D3.js library</p>
-            <p>	Built prototype  application for mobile devices  with Backbone and D3.js tree layout api</p>
-            <p>Upgraded internal framework with Bootstrap 3 </p>
-    
-    
-    
-            <h4 className="mar-tb-15">Institute for Integrative Nutrition, New York, User Interface Engineer, November 2013 - August 2014</h4>
-    
-            <p>Created  responsive website using Bootstrap 3, Less and jQuery</p>
-            <p>Utilized  google analytics API to generate page views, events and ecommerce transactions reports</p>
-            <p>Utilized social media API ( facebook, twiter)</p>
-            <p>Implemented client side form validation with Angular directives</p>
-            <p>Used GIT  versioning system</p>
-    
-            <h4 className="mar-tb-15">Equinox , New York, User Interface developer , August 2010 - October 2013</h4>
-    
-    
-            <p>Created mobile website with Responsive Web Design using Bootstrap 3</p>
-            <p>Implemented restful modular application using javascript Backbone MVC framework, Grunt and Grunt-bbb</p>
-            <p>Utilized various HTML5 API’s  - local storage, drag and drop, offline mode </p>
-            <p>Created  jQuery object oriented plugins</p>
-            <p>Implemented javascript  publisher/subscriber pattern for very rich UI application</p>
-            <p>Transformed PSD’s into cross browser compatible and valid HTML5/CSS3</p>
-            <p>Created animated widgets with Raphael JS Library</p>
-            <p>Insured cross browser and device compatibility – for Windows and Mac environments</p>
-            <p>Created html emails that render properly in desktop and mobile devices</p>
-    
-            <h4 className="mar-tb-15">Redcats USA, New York UI developer , February 2008 - March 2010  </h4>
-    
-            <p>Developed front-end web technologies that implemented a high-quality user experience for European  e-commerce websites designed in .NET  environment.</p>
-            <p>Transformed  design mockup into cross browser compatible  and valid XHTML/CSS layouts and implemented dynamic elements using  JavaScript,  AJAX  and Jquery </p>
-            <p>Analyzed web site response time and explored different factors that affect web page speed.  Worked with a team on optimizing structure, design and graphics of the websites to improve response time. </p>
-    
-            <h4 className="mar-tb-15"> Hertz, Park Ridge, NJ, UI developer, June 2007 - January 2008</h4>
-    
-            <p>Designed   and updated  web sites using Dreamweaver, CSS2,  DOM and JavaScript</p>
-            <p>Converted  html code to valid and well formed  xhtml</p>
-            <p>Tested websites on multiple browsers and corrected CSS code to achieve cross compatibility</p>
-    
-            <h4 className="mar-tb-15">Freelance UI Developer  (2006 - 2007)</h4>
-    
-            <p>Created and updated web sites using Dreamweaver MX, JavaScript , Photoshop. </p>
-            <p>Created artwork, digitally modified photography, and optimized images.</p>
-            <p>Used CSS to control the presentation of the website for different media.</p>
-            <p>Designed animated banners by utilizing Flash MX </p>
-    
-    
-            <h4 className="mar-tb-15">Assistant, Lehman College Spring 2003</h4>
-            <p>Responsibilities included mentoring Students, tutoring Java and helping students with troubleshooting assignments.</p>
-    
-        </section>     
-        </section>    
+      </div>
     </section>
-    </div>
-    </section>
-    )  
-}
+  );
+};
 
-export default Resume
-
-
-
-
-
+export default Resume;
